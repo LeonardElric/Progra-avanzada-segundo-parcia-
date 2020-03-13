9c194c8b8007578d706ace4f7bd73fde2ff9d351
EdgarLopezM <61257915+EdgarLopezM@users.noreply.github.com>
2020-03-12 21:27:19 -0600

    Add files via upload

--- ruleta.py
+++ ruleta.py
@@ -2,7 +2,7 @@ from tkinter import *
 from random import randint
 global hacer_apuesta,minimo,din_dis,color
 minimo=100
-color=["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+color=["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
 
 
 def apuesta():
@@ -30,6 +30,30 @@ def comienzo():
     hacer_apuesta.title("Apuesta")
     hacer_apuesta.withdraw()
 
+    BCR=Button(hacer_apuesta,text="rojos",command=rojos)
+    BCR.grid(row=6,column=0)
+
+    BCN=Button(hacer_apuesta,text="negros",command=negros)
+    BCN.grid(row=6,column=1)
+
+    BPAR=Button(hacer_apuesta,text="Pares",command=pares)
+    BPAR.grid(row=6,column=2)
+
+    BIPR=Button(hacer_apuesta,text="Impares",command=pares)
+    BIPR.grid(row=6,column=3)
+
+    BPAS=Button(hacer_apuesta,text="Pasa",command=pasa)
+    BPAS.grid(row=6,column=4)
+
+    BFAL=Button(hacer_apuesta,text="Falta",command=falta)
+    BFAL.grid(row=6,column=5)
+
+    NUM=Entry(hacer_apuesta,textvariable=au)
+    NUM.grid(row=6,column=6)
+
+    BUM=Button(hacer_apuesta,text="verificar numero",command=numero)
+    BUM.grid(row=6,column=7)
+        
     a=1
     color[0]="verde"
 
@@ -37,6 +61,7 @@ def comienzo():
         color[2*a]="negro"
         color[(2*a)-1]="rojo"
         a=a+1
+    color[38]="verde"
 
     if(din_dis<minimo):
         error.set("necesitas mas dinero")
@@ -55,9 +80,10 @@ def verificar():
 
     a=float(dr)
 
-    if(a<=0 and a>din_dis and a<minimo):
-        apostado.set("no puedes apostar esa cantidad \n Prueba de nuevo")
-    else:
+    if(a<=0 and a>din_dis):
+        if(a<minimo):
+            apostado.set("no puedes apostar esa cantidad \n Prueba de nuevo")
+    if(a>=minimo and a<=din_dis):
         porapostar.set("Usted est apostando: " + str(a))
         BRet=Button(ventana,text="salir",command=fin,state=DISABLED)
         BRet.grid(row=10,column=2)
@@ -67,17 +93,51 @@ def verificar():
         Dpa.config(state=DISABLED)
 
         hacer_apuesta.deiconify()
+    else:
+        apostado.set("no puedes apostar esa cantidad \n Prueba de nuevo")
         
 def todo():
+    global din_dis,minimo
+
+    porapostar.set("Usted est apostando: " + str(din_dis))
+    BRet=Button(ventana,text="salir",command=fin,state=DISABLED)
+    BRet.grid(row=10,column=2)
+
+    Ball.config(state=DISABLED)
+    Brev.config(state=DISABLED)
+    Dpa.config(state=DISABLED)
+
+    hacer_apuesta.deiconify()
+
+def rojos():
     pass
 
+def negros():
+    pass
+
+def pares():
+    pass
+    
+def impares():
+    pass
+
+def pasa():
+    pass
+              
 def fin():
     pass
-        
+
+def falta():
+    pass
+
+def numero():
+    pass
+              
 ventana=Tk()
 
 m=StringVar()
 da=StringVar()
+au=StringVar()
 error=StringVar()
 bienvenido=StringVar()
 apostado=StringVar()
@@ -106,14 +166,5 @@ Brev.grid(row=9,column=2)
 Ball=Button(ventana,text="Apostar todo",command=todo,state=DISABLED)
 Ball.grid(row=9,column=3)
 
-
-
     
 ventana.mainloop()
-
-##hacer_apuesta=Toplevel()
-##        hacer_apuesta.title("Apuesta")
-##        BI.config(state=DISABLED)
-##        dinero.config(state=DISABLED)
-##        bienvenido.set("Usted esta jugando")
-##        BRet=Button(ventana,text="salir",command=fin,state=DISABLED)
